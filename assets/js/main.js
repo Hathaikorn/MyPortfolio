98ff203d8113ec0422c222acf64be47a2523472a
Hathaikorn <corner12345678910@gmail.com>
2023-04-20 02:23:08 +0700

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,7 +23,7 @@ const navLinks = document.querySelectorAll(".nav-link");
 
 function linkAction() {
   const navMenu = document.getElementById("nav-menu");
-  // when we click on each nav link, we remove the show menu class
+
   navMenu.classList.remove("show-menu");
 }
 navLinks.forEach((n) => n.addEventListener("click", linkAction));
@@ -31,41 +31,27 @@ navLinks.forEach((n) => n.addEventListener("click", linkAction));
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 function scrollHeader() {
   const header = document.getElementById("header");
-  // when the scroll is greater than 80 viewport height, add the class scroll header to the tag header
+
   if (this.scrollY >= 80) header.classList.add("scroll-header");
   else header.classList.remove("scroll-header");
 }
 window.addEventListener("scroll", scrollHeader);
 
-/*=============== TESTIMONIAL SWIPER ===============*/
-var swiper = new Swiper(".testimonial-wrapper", {
-  spaceBetween: 30,
-  loop: "true",
-
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
-
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-// get all sections that have an id defined
 const sections = document.querySelectorAll("section[id]");
 
-// add an event listener listening for scroll
 window.addEventListener("scroll", navHighlighter);
 
 function navHighlighter() {
   // get current scroll position
   let scrollY = window.pageYOffset;
-  // Now we loop through sections to get height, top and ID values for each
+
   sections.forEach((current) => {
     const sectionHeight = current.offsetHeight;
     const sectionTop = current.offsetTop - 58,
       sectionId = current.getAttribute("id");
-    /* - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
-    - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector */
+
     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
       document
         .querySelector(".nav-menu a[href*=" + sectionId + "]")
